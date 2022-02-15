057368052e69caaea0b3e119f3b3d4752e52c726
Ali-consciousroad <ali_dindar@live.be>
2022-02-15 21:17:20 +0100

    Provide validation feedback

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnterdName] = useState("");
+  const [enteredNameIsValid, setEnterdNameIsValid] = useState(true); 
 
   const nameInputChangeHandler = (event) => {
     setEnterdName(event.target.value);
@@ -12,8 +13,11 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     if (enteredName.trim() === '') {
+      setEnterdNameIsValid(false);  
       return;
     }
+
+    setEnterdNameIsValid(true);
 
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value; 
@@ -24,9 +28,13 @@ const SimpleInput = (props) => {
     setEnterdName('');
   };
 
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -35,6 +43,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
